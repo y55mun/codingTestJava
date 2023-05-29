8fe337b06913aba82af7033cba68cd3182c0ee57
User <y55mun33@gmail.com>
2023-05-29 16:46:48 +0900

    수정

--- codingTest/src/codingTest/c1_15651.java
+++ codingTest/src/codingTest/c1_15651.java
@@ -4,18 +4,18 @@ import java.io.*;
 import java.util.*;
 
 public class c1_15651 {
-	static StringBuilder sb = new StringBuilder();
-	
-	static void input() {
-		FastReader scan = new FastReader();
-		N = scan.nextInt();
-		M = scan.nextInt();
-		selected = new int[M+1];
-	}
-	
-	static int N, M;
-	static int[] selected;
-	
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        M = scan.nextInt();
+        selected = new int[M + 1];
+    }
+
+    static int N, M;
+    static int[] selected;
+
     // Recurrence Function (재귀 함수)
     // 만약 M 개를 전부 고름   => 조건에 맞는 탐색을 한 가지 성공한 것!
     // 아직 M 개를 고르지 않음 => k 번째부터 M번째 원소를 조건에 맞게 고르는 모든 방법을 시도한다.
@@ -35,58 +35,59 @@ public class c1_15651 {
             }
         }
     }
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		input();
-		
-		rec_func(1);
-		System.out.println(sb.toString());
-	}
-	
-    static class FastReader {
-    	BufferedReader br;
-    	StringTokenizer st;
-    	
-    	public FastReader() {
-    		br = new BufferedReader(new InputStreamReader(System.in));
-    	}
-    	
-    	public FastReader(String s) throws FileNotFoundException {
-    		br = new BufferedReader (new FileReader(new File(s)));
-    	}
-    	
-    	String next() {
-    		while (st == null || !st.hasMoreElements()) {
-    			try {
-    				st = new StringTokenizer(br.readLine());
-    			} catch (IOException e) {
-    				e.printStackTrace();
-    			}
-    		}
-    		return st.nextToken();
-    	}
-    	
-    	int nextInt() {
-    		return Integer.parseInt(next());
-    	}
-    	
-    	long nextLong() {
-    		return Long.parseLong(next());
-    	}
-    	
-    	double nextDouble() {
-    		return Double.parseDouble(next());
-    	}
-    	
-    	String nextLine() {
-    		String str = "";
-    		try {
-    			str = br.readLine();
-    		} catch (IOException e) {
-    			e.printStackTrace();
-    		}
-    		return str;
-    	}
+
+    public static void main(String[] args) {
+        input();
+
+        // 1 번째 원소부터 M 번째 원소를 조건에 맞는 모든 방법을 찾아줘
+        rec_func(1);
+        System.out.println(sb.toString());
     }
 
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
 }
