3f2d6810fe3c93e86b61ad0c050f9dc1d2f46648
User <y55mun33@gmail.com>
2023-06-02 00:48:39 +0900

    dfs, bfs 구현

--- codingTest/src/practice/c7_1260.java
+++ codingTest/src/practice/c7_1260.java
@@ -30,8 +30,45 @@ public class c7_1260 {
 		bfs(v);
 		System.out.println(sb);
 	}
-
 	
+	static void dfs(int x) {
+		// x를 방문
+		visit[x] = true;
+		sb.append(x).append(' ');
+		
+		// x에서 갈 수 있는 곳들을 작은 번호부터 모두 방문한다.
+		for(int y = 1; y<=n; y++) {
+			if(adj[x][y] == 0) continue;
+			
+			// y를 이미 갈 수 있다는 사실을 알면, 굳이 갈 필요는 없다
+			if(visit[y]) continue;
+			
+			// y에서 갈 수 있는 곳들 확인
+			dfs(y);
+		}
+	}
+	
+	static void bfs(int start) {
+		Queue<Integer> que = new LinkedList<>();
+		
+		//start 는 방문 가능한 점 이므로 que 에 넣어줌
+		que.add(start);
+		visit[start] = true;	// start 를 갈 수 있다고 표시(중요)
+		
+		while(!que.isEmpty()) {
+			int x = que.poll();
+			
+			sb.append(x).append(' ');
+			for(int y=1; y<=n; y++) {
+				if(adj[x][y] == 0) continue;
+				if(visit[y]) continue;	// x에서 y를 갈 수는 있지만, 이미 탐색한 점이라면 무시
+				
+				// y를 갈 수 있으니 que 에 추가하고, visit 처리
+				que.add(y);
+				visit[y] = true;
+			}
+		}
+	}
 	
 	public static void main(String[] args) {
 		input();
