630d605e361e4c4868afab4f0934d22b61ecc111
User <y55mun33@gmail.com>
2023-09-29 18:00:27 +0900

    new

--- codingTest/src/FastCampus/c1_15651.java
+++ codingTest/src/FastCampus/c1_15651.java
@@ -3,52 +3,9 @@ package FastCampus;
 import java.io.*;
 import java.util.*;
 
+
 public class c1_15651 {
 	static StringBuilder sb = new StringBuilder();
-	
-	static class FastReader {
-		BufferedReader br;
-		StringTokenizer st;
-		
-		public FastReader() {
-			br = new BufferedReader(new InputStreamReader(System.in));
-		}
-		
-		public FastReader(String s) throws FileNotFoundException {
-			br = new BufferedReader(new FileReader(new File(s)));
-		}
-		
-		String next() {
-			while (st == null || !st.hasMoreElements()) {
-				try {
-					st = new StringTokenizer(br.readLine());
-				} catch(IOException e) {
-					e.printStackTrace();
-				}
-			}
-			return st.nextToken();
-		}
-		
-		int nextInt() {
-			return Integer.parseInt(next());
-		}
-		long nextLong() {
-			return Long.parseLong(next());
-		}
-		
-		double nextDouble() {
-			return Double.parseDouble(next());
-		}
-		String nextLine() {
-			String str = "";
-			try {
-				str = br.readLine();
-			} catch(IOException e) {
-				e.printStackTrace();
-			}
-			return str;
-		}
-	}
 	
 	static void input() {
 		FastReader scan = new FastReader();
@@ -56,17 +13,17 @@ public class c1_15651 {
 		m = scan.nextInt();
 		selected = new int[m+1];
 	}
-	
 	static int n,m;
 	static int[] selected;
 	
 	static void rec_func(int k) {
-		if (k == m+1) {
+		if (k==m+1) {
 			for (int i=1; i<=m; i++) sb.append(selected[i]).append(' ');
 			sb.append('\n');
 		} else {
-			for (int cand = 1; cand <= n; cand++) {
-				selected [k] = cand;
+			for(int cand=1; cand<=n; cand++) {
+				selected[k] = cand;
+				
 				rec_func(k+1);
 				selected[k] = 0;
 			}
@@ -77,5 +34,45 @@ public class c1_15651 {
 		input();
 		rec_func(1);
 		System.out.println(sb.toString());
+	}
+	
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
 	}
 }
