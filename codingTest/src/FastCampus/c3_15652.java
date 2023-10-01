2f096f76d556d9ae649cc7320daefd953d675ae5
User <y55mun33@gmail.com>
2023-10-01 22:35:32 +0900

    다시

--- codingTest/src/FastCampus/c3_15652.java
+++ codingTest/src/FastCampus/c3_15652.java
@@ -3,30 +3,27 @@ package FastCampus;
 import java.io.*;
 import java.util.*;
 
-// 중복 허용해서 고르기
-// 비내림차순: ex. 2 3, 3 2도 가능했었는데 => 2 3 으로 됨!
-
 public class c3_15652 {
 	static StringBuilder sb = new StringBuilder();
 	
 	static void input() {
 		FastReader scan = new FastReader();
-		N = scan.nextInt();
-		M = scan.nextInt();
-		selected = new int[M+1];
+		n = scan.nextInt();
+		m = scan.nextInt();
+		selected = new int[m+1];
 	}
-
-	static int N, M;
+	
+	static int n,m;
 	static int[] selected;
 	
 	static void rec_func(int k) {
-		if (k == M+1) {
-			for (int i=1; i<=M; i++) sb.append(selected[i]).append(' ');
+		if (k == m+1) {
+			for (int i=1; i<=m; i++) sb.append(selected[i]).append(' ');
 			sb.append('\n');
 		} else {
 			int start = selected[k-1];
-			if (start ==0) start = 1;
-			for(int cand=start; cand <= N; cand++) {
+			if (start == 0) start = 1;
+			for(int cand=start; cand<=n; cand++) {
 				selected[k] = cand;
 				rec_func(k+1);
 				selected[k] = 0;
@@ -36,7 +33,6 @@ public class c3_15652 {
 	
 	public static void main(String[] args) {
 		input();
-		
 		rec_func(1);
 		System.out.println(sb.toString());
 	}
@@ -49,20 +45,21 @@ public class c3_15652 {
 			br = new BufferedReader(new InputStreamReader(System.in));
 		}
 		
-		public FastReader(String s) throws FileNotFoundException{
+		public FastReader(String s) throws FileNotFoundException {
 			br = new BufferedReader(new FileReader(new File(s)));
 		}
 		
 		String next() {
-			while (st == null || !st.hasMoreElements()) {
+			while(st == null || !st.hasMoreElements()) {
 				try {
 					st = new StringTokenizer(br.readLine());
-				} catch(IOException e) {
+				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 			return st.nextToken();
 		}
+		
 		int nextInt() {
 			return Integer.parseInt(next());
 		}
@@ -73,15 +70,13 @@ public class c3_15652 {
 			return Double.parseDouble(next());
 		}
 		String nextLine() {
-			String str ="";
+			String str = "";
 			try {
 				str = br.readLine();
-			} catch (IOException e) {
+			} catch(IOException e) {
 				e.printStackTrace();
 			}
 			return str;
 		}
-		
 	}
-
 }
