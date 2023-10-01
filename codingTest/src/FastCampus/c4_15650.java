f2217be7e9a3d66b4486dccb202391cae0c6ed44
User <y55mun33@gmail.com>
2023-10-01 22:53:29 +0900

    다시

--- codingTest/src/FastCampus/c4_15650.java
+++ codingTest/src/FastCampus/c4_15650.java
@@ -3,30 +3,25 @@ package FastCampus;
 import java.io.*;
 import java.util.*;
 
-import FastCampus.c2_15649.FastReader;
-
-// 중복 없이 고르기
-
 public class c4_15650 {
-
 	static StringBuilder sb = new StringBuilder();
 	
 	static void input() {
 		FastReader scan = new FastReader();
-		N = scan.nextInt();
-		M = scan.nextInt();
-		selected = new int[M+1];
+		n = scan.nextInt();
+		m = scan.nextInt();
+		selected = new int[m+1];
 	}
 	
-	static int N, M;
+	static int n,m;
 	static int[] selected;
 	
 	static void rec_func(int k) {
-		if (k == M+1) {
-			for(int i=1; i<=M; i++) sb.append(selected[i]).append(' ');
+		if (k == m+1) {
+			for(int i=1; i<=m; i++) sb.append(selected[i]).append(' ');
 			sb.append('\n');
 		} else {
-			for (int cand = selected[k-1]; cand <= N; cand ++) {
+			for (int cand=selected[k-1]; cand<=n; cand++) {
 				selected[k] = cand;
 				rec_func(k+1);
 				selected[k] = 0;
@@ -34,10 +29,8 @@ public class c4_15650 {
 		}
 	}
 	
-	
 	public static void main(String[] args) {
 		input();
-		
 		rec_func(1);
 		System.out.println(sb.toString());
 	}
@@ -49,20 +42,19 @@ public class c4_15650 {
 		public FastReader() {
 			br = new BufferedReader(new InputStreamReader(System.in));
 		}
-		public FastReader(String s) throws FileNotFoundException{
+		public FastReader(String s) throws FileNotFoundException {
 			br = new BufferedReader(new FileReader(new File(s)));
 		}
 		String next() {
-			while (st == null || ! st.hasMoreElements()) {
+			while(st == null || !st.hasMoreElements()) {
 				try {
 					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
+				} catch(IOException e) {
 					e.printStackTrace();
 				}
 			}
 			return st.nextToken();
 		}
-		
 		int nextInt() {
 			return Integer.parseInt(next());
 		}
@@ -81,6 +73,5 @@ public class c4_15650 {
 			}
 			return str;
 		}
-		
 	}
 }
