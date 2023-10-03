c778b6417989770a5ef41b348d05afefb5814015
User <y55mun33@gmail.com>
2023-10-04 02:32:15 +0900

    완료

--- codingTest/src/brute_force/c5_14888.java
+++ codingTest/src/brute_force/c5_14888.java
@@ -7,6 +7,53 @@ public class c5_14888 {
 	static FastReader scan = new FastReader();
 	static StringBuilder sb = new StringBuilder();
 	
+	static void input() {
+		n = scan.nextInt();
+		nums = new int[n+1];
+		operators = new int[5];
+		for(int i=1; i<=n; i++) nums[i]=scan.nextInt();
+		for(int i=1; i<=4; i++) operators[i]=scan.nextInt();
+		
+		max = Integer.MIN_VALUE;
+		min = Integer.MAX_VALUE;
+	}
+	static int n, max, min;
+	static int[] nums, operators;
+	
+	
+	static int calculator(int operand1, int operator, int operand2) {
+		if (operator == 1) // +
+			return operand1 + operand2;
+		else if (operator == 2) // -
+			return operand1 - operand2;
+		else if (operator == 3) // *
+			return operand1 * operand2;
+		else // /
+			return operand1 / operand2;
+	}
+	
+	static void rec_func(int k, int value) {
+		if(k == n) {
+			max = Math.max(max, value);
+			min = Math.min(min, value);
+		} else {
+			for(int cand=1; cand<=4; cand++) {
+				if(operators[cand] >= 1) {
+					operators[cand]--;
+					rec_func(k+1, calculator(value, cand, nums[k+1]));
+					operators[cand]++;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		input();
+		rec_func(1, nums[1]);
+		sb.append(max).append('\n').append(min);
+		System.out.println(sb.toString());
+	}
+	
 	static class FastReader {
 		BufferedReader br;
 		StringTokenizer st;
@@ -21,7 +68,7 @@ public class c5_14888 {
 			while (st == null || !st.hasMoreElements()) {
 				try {
 					st = new StringTokenizer(br.readLine());
-				} catch (IOException e) {
+				}catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
@@ -40,63 +87,11 @@ public class c5_14888 {
 			String str = "";
 			try {
 				str = br.readLine();
-			} catch (IOException e) {
+			} catch(IOException e) {
 				e.printStackTrace();
 			}
 			return str;
 		}
-	}
-	
-	static void input() {
-		n = scan.nextInt();
-		nums = new int[n+1];
-		operators = new int[5];
-		for (int i=1; i<=n; i++) nums[i] = scan.nextInt();
-		for (int i=1; i<=4; i++) operators[i] = scan.nextInt();
-		
-		max = Integer.MIN_VALUE;
-		min = Integer.MAX_VALUE;
-	}
-	
-	static int n, max, min;
-	static int[] nums, operators;
-	
-	static int calculator(int operand1, int operator, int operand2) {
-		if (operator == 1) // +
-			return operand1 + operand2;
-		else if (operator == 2) // -
-			return operand1 - operand2;
-		else if (operator == 3) // *
-			return operand1 * operand2;
-		else
-			return operand1 / operand2;
 		
 	}
-	
-	
-	// order[1...n-1] 에 연산자들이 순서대로 저장
-	static void rec_func(int k, int value) {
-		if (k==n) {
-			max = Math.max(max, value);
-			min = Math.min(min, value);
-		} else {	// k번째 연산자를 무엇으로 할 것인지
-			for (int cand = 1; cand<=4; cand++) {
-				if(operators[cand] >= 1) {
-					operators[cand]--;
-					rec_func(k+1, calculator(value, cand, nums[k+1]));
-					operators[cand]++;
-				}
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		input();
-		rec_func(1, nums[1]);
-		sb.append(max).append('\n').append(min);
-		System.out.println(sb.toString());
-		
-	}
-
 }
